58fab3bb712643ede07f8d3ef01fe093053df842
pinyue <bca6130836@gmail.com>
2024-01-28 21:25:22 +0800

    產品設定

---
+++ Team1/Models/EFModels/Attraction.cs
@@ -0,0 +1,54 @@
+namespace Team1.Models.EFModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Attraction
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Attraction()
+        {
+            AttractionContents = new HashSet<AttractionContent>();
+            AttractionImages = new HashSet<AttractionImage>();
+            AttractionTickets = new HashSet<AttractionTicket>();
+            PackageAttractionItems = new HashSet<PackageAttractionItem>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public int AttractionCategoryId { get; set; }
+
+        public int CityId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string MainImage { get; set; }
+
+        public virtual AttractionCategory AttractionCategory { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AttractionContent> AttractionContents { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AttractionImage> AttractionImages { get; set; }
+
+        public virtual City City { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AttractionTicket> AttractionTickets { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PackageAttractionItem> PackageAttractionItems { get; set; }
+    }
+}
