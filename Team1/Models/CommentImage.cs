5600a4beb32175596bcfba33b24e3196713fd4ec
Kelsier1027 <w6886423e@gmail.com>
2024-01-29 20:56:49 +0800

    更新 EFModels

---
+++ Team1/Models/CommentImage.cs
@@ -0,0 +1,21 @@
+namespace Team1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class CommentImage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FileName { get; set; }
+
+        public int CommentId { get; set; }
+
+        public virtual Comment Comment { get; set; }
+    }
+}
