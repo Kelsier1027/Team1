1f8549665e2dd55361915d4440de28ee1d1c7dd1
LAPTOP-928JOO12\asd22 <asd2264989@gmail.com>
2024-01-30 00:15:44 +0800

    新增側欄飯店及訂單，Hotelcontroller及view，印出資料

---
+++ Team1/Controllers/HotelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team1.Models;
+
+namespace Team1.Controllers
+{
+    public class HotelController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            var data = db.Hotels.ToList();
+            return View(data);
+        }
+    }
+}
