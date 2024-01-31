1835d1d6ddafe860d47a0e2478f1ede9588664cd
zhen <j860617@gmail.com>
2024-01-31 10:47:57 +0800

    還原

--- Team1/個人/Chih/Models/DTO/AttractionPagingDTO.cs
+++ Team1/個人/Chih/Models/DTO/AttractionPagingDTO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using Team1.Models.EFModels;
+using Team1.Models.EFModel;
 
 namespace Team1.Models.DTO
 {
