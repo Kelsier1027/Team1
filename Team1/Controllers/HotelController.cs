3dc4e54de77cefc19892db299926e704d1a6052a
LAPTOP-928JOO12\asd22 <asd2264989@gmail.com>
2024-02-02 08:38:08 +0800

    新增房型 刪除飯店 以及顯示JSON文字

--- Team1/Controllers/HotelController.cs
+++ Team1/Controllers/HotelController.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using Team1.Models;
+using Team1.Models.EFModels;
 
 namespace Team1.Controllers
 {
+    public class Facilities
+    {
+        public List<int> FacilityIds { get; set; }
+
+    }
+
+
     public class HotelController : Controller
     {
         private AppDbContext db = new AppDbContext();
@@ -14,31 +22,126 @@ namespace Team1.Controllers
         public ActionResult Index()
         {
             var data = db.Hotels.ToList();
+            var hotelCategories = db.HotelCategories.ToList();
+
+            foreach (var singleData in data)
+            {
+                if (string.IsNullOrEmpty(singleData.HotelFacilities))
+                {
+                    continue;
+                }
+
+                Facilities obj = JsonConvert.DeserializeObject<Facilities>(singleData.HotelFacilities);
+                string detail = "<ul>";
+
+                if (obj?.FacilityIds != null)
+                {
+                    foreach (var facilityId in obj.FacilityIds)
+                    {
+                        var facility = hotelCategories.FirstOrDefault(x => x.Id == facilityId);
+                        if (facility != null)
+                        {
+                            detail += $"<li>{facility.JAON}</li>";
+                        }
+                    }
+                }
+                detail += "</ul>";
+                singleData.HotelFacilities = detail;
+            }
+
             return View(data);
         }
 
         // GET: Hotel/Create
         public ActionResult Create()
         {
+            Hotel hotel = new Hotel();
+            var hotelCategories = db.HotelCategories.ToList();
             ViewBag.DistrictId = new SelectList(db.Districts, "Id", "Name");
             ViewBag.HotelTypeId = new SelectList(db.HotelTypes, "Id", "Name");
-            return View();
+             ViewBag.Categories = hotelCategories;
+            return View(hotel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,DistrictId,Address,Grade,Phone,Level,MainImage,CheckinStart,CheckinEnd,CheckoutStart,CheckoutEnd,AddBedFee,BreakfastPrice,HotelTypeId,HotelFacilities,Email")] Hotel hotel)
+        public ActionResult Create([Bind(Include = "Id,Name,DistrictId,Address,Grade,Phone,Level,MainImage,CheckinStart,CheckinEnd,CheckoutStart,CheckoutEnd,AddBedFee,BreakfastPrice,HotelTypeId,HotelFacilities,Email")] Hotel hotel, int[] selectedCategories)
         {
             if (ModelState.IsValid)
             {
+                // 序列化成 JSON 格式
+                string json = JsonConvert.SerializeObject(new { FacilityIds = hotel.SelectedCategoryIds });
+
+                // 指定給 HotelFacilities 欄位
+                hotel.HotelFacilities = json;
                 db.Hotels.Add(hotel);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                Session["HotelId"] = hotel.Id;
+                return RedirectToAction("CreateRoom");
             }
 
             ViewBag.DistrictId = new SelectList(db.Districts, "Id", "Name", hotel.DistrictId);
             ViewBag.HotelTypeId = new SelectList(db.HotelTypes, "Id", "Name", hotel.HotelTypeId);
+            ViewBag.Categories = db.HotelCategories.ToList();  // 重新設置類別
             return View(hotel);
+        }
+        public ActionResult CreateRoom()
+        {
+            var hotelId = Session["HotelId"] as int?;
+            if (hotelId == null)
+            {
+                return RedirectToAction("index");
+            }
+
+            var model = new HotelRoom { HotelId = hotelId.Value };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateRoom(HotelRoom hotelRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HotelRooms.Add(hotelRoom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hotelRoom);
+        }
+
+        // GET: Hotel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: Hotel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hotel hotel = db.Hotels.Find(id);
+            db.Hotels.Remove(hotel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public JsonResult GetFacilities()
+        {
+            var facilities = db.HotelCategories
+                .Select(e => e.JAON)
+                .Distinct()
+                .ToList();
+
+            return Json(facilities, JsonRequestBehavior.AllowGet);
         }
     }
 }
