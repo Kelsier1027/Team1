74a26249ed58335d53cf93be3d9fb605a14d6d16
LAPTOP-928JOO12\asd22 <asd2264989@gmail.com>
2024-02-05 01:03:42 +0800

    飯店頁面點擊房間明細對跳轉到Hotel Room頁面

---
+++ Team1/Controllers/HotelRoomController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team1.Models.EFModels;
+
+namespace Team1.Controllers
+{
+    public class HotelRoomController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: HotelRoom
+        public ActionResult Index(int? id)
+        {
+            // 使用 LINQ 查询来找出所有匹配 hotelId 的 HotelRooms
+            var hotelRooms = db.HotelRooms.Where(h => h.HotelId == id).ToList();
+
+            return View(hotelRooms);
+        }
+    }
+}
